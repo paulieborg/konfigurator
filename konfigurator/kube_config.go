6a0c42452fdaddfd60115ef40734782bec2a7ff5
Gustavo Hoirisch <gustavo.hoirisch@myob.com>
2017-08-14 10:28:33 +1000

    added comments to exported kube_config functions

--- konfigurator/kube_config.go
+++ konfigurator/kube_config.go
@@ -5,6 +5,7 @@ import (
 	"io"
 )
 
+// KubeConfig holds the information necessary to generate a Kubernetes configuration file which icludes the server's CA, the api url and where to write the file to
 type KubeConfig struct {
 	CA     string
 	URL    string
@@ -39,6 +40,7 @@ type configData struct {
 	Token string
 }
 
+// Returns an initialized KubeConfig struct
 func NewKubeConfig(ca, url string, output io.ReadWriteCloser) (*KubeConfig, error) {
 	tmpl, err := template.New("config").Parse(content)
 	if err != nil {
@@ -53,6 +55,7 @@ func NewKubeConfig(ca, url string, output io.ReadWriteCloser) (*KubeConfig, erro
 	}, nil
 }
 
+// This function executes the writing of the config to the appropriate location (os.Stdout, os.File, etc...)
 func (k *KubeConfig) Generate(token string) error {
 	defer k.Output.Close()
 	err := k.tmpl.Execute(k.Output, configData{
